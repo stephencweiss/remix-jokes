ae3c2cf9e2558f0a1fdc8616879e0b78da088928
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-02-19 12:43:02 -0600

    Add joke submission

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,21 @@
+import type { ActionArgs } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { Form } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+
+export async function action({ request }: ActionArgs) {
+  const body = await request.formData();
+  const name = String(body.get('name'));
+  const content = String(body.get('content'));
+  if (!name || !content) {
+    // TODO: error handle?
+    throw new Error('Form not submitted correctly!');
+  }
+  const data = { name, content };
+  const joke = await db.joke.create({ data });
+  return redirect(`/jokes/${joke.id}`);
+}
+
 export default function NewJokeRoute() {
   return (
     <body>
