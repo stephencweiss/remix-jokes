8dec709225aa5c4303f47c7f15e3f52d7b5225c0
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-02-19 20:05:16 -0600

    Replace cookie sessions with db sessions

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,4 +1,5 @@
-import { createCookieSessionStorage, redirect } from '@remix-run/node';
+import type { CookieOptions, SessionData } from '@remix-run/node';
+import { createSessionStorage, redirect } from '@remix-run/node';
 import * as bcrypt from 'bcryptjs'
 import { db } from "./db.server"
 
@@ -6,8 +7,8 @@ type LoginForm = {
   username: string;
   password: string;
 }
-export async function login({ username, password }: LoginForm) {
 
+export async function login({ username, password }: LoginForm) {
   const user = await db.user.findUnique({ where: { username } })
   if (!user) {
     return null;
@@ -26,22 +27,55 @@ if (!sessionSecret) {
   throw new Error("SESSION_SECRET must be set");
 }
 
-const storage = createCookieSessionStorage({
-  cookie: {
-    name: '__session',
-    secure: process.env.NODE_ENV === 'production',
-    secrets: [sessionSecret],
-    sameSite: 'lax',
-    path: '/',
-    maxAge: 60 * 60 * 24 * 30,
-    httpOnly: true,
+const cookie: CookieOptions & { name: string } = {
+  name: '__session',
+  secure: process.env.NODE_ENV === 'production',
+  secrets: [sessionSecret],
+  sameSite: 'lax',
+  path: '/',
+  maxAge: 60 * 60 * 24 * 30,
+  httpOnly: true,
+}
 
-  }
-})
+function createDatabaseSessionStorage({
+  cookie,
+}: { cookie: CookieOptions }) {
+  // Configure your database client...
+
+  return createSessionStorage({
+    cookie,
+    async createData(data: SessionData, expires?: Date) {
+      console.log({data})
+      const session = await db.session.create({ data: { userId: data.userId, expiresAt: expires } });
+      return session.id;
+    },
+    async readData(id) {
+      const session = await db.session.findUnique({
+        where: { id },
+        select: { userId: true }
+      });
+      return session;
+    },
+    async updateData(id, data, expires) {
+      await db.session.update({
+        where: { id },
+        data: {
+          ...(expires ? { expiresAt: expires } : {}),
+          ...data
+        }
+      })
+    },
+    async deleteData(id) {
+      await db.session.delete({ where: { id } });
+    },
+  })
+};
+const storage = createDatabaseSessionStorage({ cookie });
 
 export async function createUserSession(userId: string, redirectTo: string) {
   const session = await storage.getSession();
   session.set('userId', userId)
+
   return redirect(redirectTo, {
     headers: {
       "Set-Cookie": await storage.commitSession(session),
@@ -52,17 +86,19 @@ export async function createUserSession(userId: string, redirectTo: string) {
 export async function getUserSession(request: Request) {
   return await storage.getSession(request.headers.get("Cookie"))
 }
+
 export async function getUserId(request: Request) {
   const session = await getUserSession(request);
   const userId = session.get('userId')
   if (!userId || typeof userId !== 'string') return null;
   return userId
 }
+
 export async function requireUserId(
   request: Request,
   redirectTo: string = new URL(request.url).pathname) {
-  const session = await getUserSession(request);
-  const userId = session.get('userId')
+  const userId = await getUserId(request);
+
   if (!userId || typeof userId !== 'string') {
     const searchParams = new URLSearchParams([
       ["redirectTo", redirectTo]
@@ -70,6 +106,4 @@ export async function requireUserId(
     throw redirect(`/login?${searchParams}`);
   }
   return userId
-
-
 }
