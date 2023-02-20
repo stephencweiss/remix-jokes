f38a331db284f541491cfb65a74938c77c310a26
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-02-19 20:31:30 -0600

    Add safe logout
    Logging out via a GET is a poor security practice as it's a stateful
    operation.
    
    See OWASP here: https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html
    
    > Do not use GET requests for state changing operations.
    > - If for any reason you do it, protect those resources against CSRF
    
    The easiest way to avoid this is to change the Logout from a loader that
    GETs a state changing operation into a form that POSTs.

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,5 +1,4 @@
-import type { ActionArgs, LoaderArgs } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
+import type { ActionArgs } from '@remix-run/node';
 
 import { logout } from '~/utils/session.server';
 
@@ -7,7 +6,15 @@ export const action = async ({ request }: ActionArgs) => {
   return logout(request);
 };
 
-export const loader = async ({request}: LoaderArgs) => {
-  await logout(request)
-  return redirect('/');
-};
+export default function LogoutRoute() {
+  // TODO: Add logic to determine if you're logged in and change the language if you're not logged in.
+  return (
+    <div className="user-info">
+      <form action="/logout" method="post">
+        <button type="submit" className="button">
+          Logout
+        </button>
+      </form>
+    </div>
+  );
+}
